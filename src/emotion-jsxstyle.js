08e2a4b33e24a6c2685c6ae2ae1dd8b42a2fa535
Jared Palmer <jared@palmer.net>
2017-11-07 11:59:27 -0500

    Refactor to factory function

--- src/emotion-jsxstyle.js
+++ src/emotion-jsxstyle.js
@@ -1,52 +1,18 @@
 import React from 'react';
 import { css as EmotionCSS } from 'react-emotion';
 
-export const Box = ({
-  component = 'div',
-  className,
-  css,
-  props,
-  children,
-  ...rest
-}) => {
-  return React.createElement(
-    component,
-    {
-      className: EmotionCSS([{ ...css, ...rest }, className]),
-      ...props,
-    },
-    children
-  );
-};
+import { jsxstyleFactory } from './jsxstyleFactory';
 
-export const Block = p => <Box display="block" {...p} />;
+const cx = (css, styles, className) =>
+  EmotionCSS([{ ...css, ...styles }, className]);
 
-Block.displayName = 'Block';
-
-export const Inline = p => <Box display="Inline" {...p} />;
-
-Inline.displayName = 'Inline';
-
-export const InlineBlock = p => <Box display="inline-block" {...p} />;
-
-InlineBlock.displayName = 'InlineBlock';
-
-export const Flex = p => <Box display="flex" {...p} />;
-
-Flex.displayName = 'Flex';
-
-export const InlineFlex = p => <Box display="inline-flex" {...p} />;
-
-InlineFlex.displayName = 'InlineFlex';
-
-export const Grid = p => <Box display="grid" {...p} />;
-
-Grid.displayName = 'Grid';
-
-export const Row = p => <Flex flexDirection="row" {...p} />;
-
-Row.displayName = 'Row';
-
-export const Col = p => <Flex flexDirection="column" {...p} />;
-
-Col.displayName = 'Col';
+const jsxstyle = jsxstyleFactory(cx);
+export const Box = jsxstyle.Box;
+export const Flex = jsxstyle.Flex;
+export const InlineFlex = jsxstyle.InlineFlex;
+export const Inline = jsxstyle.Inline;
+export const InlineBlock = jsxstyle.nlineBlock;
+export const Block = jsxstyle.Block;
+export const Grid = jsxstyle.Grid;
+export const Col = jsxstyle.Col;
+export const Row = jsxstyle.Row;
---
+++ src/jsxstyleFactory.js
@@ -0,0 +1,67 @@
+import React from 'react';
+
+export const jsxstyleFactory = fn => {
+  const Box = ({
+    component = 'div',
+    className,
+    css,
+    props,
+    children,
+    ...rest
+  }) => {
+    return React.createElement(
+      component,
+      {
+        className: fn(css, rest, className),
+        ...props,
+      },
+      children
+    );
+  };
+
+  Box.displayName = 'Box';
+
+  const Block = p => <Box display="block" {...p} />;
+
+  Block.displayName = 'Block';
+
+  const Inline = p => <Box display="Inline" {...p} />;
+
+  Inline.displayName = 'Inline';
+
+  const InlineBlock = p => <Box display="inline-block" {...p} />;
+
+  InlineBlock.displayName = 'InlineBlock';
+
+  const Flex = p => <Box display="flex" {...p} />;
+
+  Flex.displayName = 'Flex';
+
+  const InlineFlex = p => <Box display="inline-flex" {...p} />;
+
+  InlineFlex.displayName = 'InlineFlex';
+
+  const Grid = p => <Box display="grid" {...p} />;
+
+  Grid.displayName = 'Grid';
+
+  const Row = p => <Flex flexDirection="row" {...p} />;
+
+  Row.displayName = 'Row';
+
+  const Col = p => <Flex flexDirection="column" {...p} />;
+
+  Col.displayName = 'Col';
+
+  return {
+    Box,
+    Flex,
+    InlineFlex,
+    Inline,
+    InlineBlock,
+    Block,
+    Grid,
+    Col,
+    Row,
+  };
+};
